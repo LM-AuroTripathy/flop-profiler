3ef3af6cb641afacf3a6808f464895d35b603a5b
Auro Tripathy <auro@lightmatter.co>
2022-08-09 13:27:50 -0700

    init

--- alexnet_profiler.py
+++ alexnet_profiler.py
@@ -1,3 +1,4 @@
+# https://www.deepspeed.ai/tutorials/flops-profiler/
 import torchvision.models as models
 import torch
 from deepspeed.profiling.flops_profiler import get_model_profile
@@ -8,7 +9,7 @@ with torch.cuda.device(0):
     flops, macs, params = get_model_profile(model=model, # model
                                     input_shape=(batch_size, 3, 224, 224), # input shape to the model. If specified, the model takes a tensor with this shape as the only positional argument.
                                     args=None, # list of positional arguments to the model.
-                                    kwargs={"num_classes": 10}, # dictionary of keyword arguments to the model.
+                                    kwargs=None, # dictionary of keyword arguments to the model.
                                     print_profile=True, # prints the model graph with the measured profile attached to each module
                                     detailed=True, # print the detailed profile
                                     module_depth=-1, # depth into the nested modules, with -1 being the inner most modules
